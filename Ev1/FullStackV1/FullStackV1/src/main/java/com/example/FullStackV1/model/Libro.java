bb07ab894db0c05a10b8ca97e6862c940ada046a
Juan Pablo Jofre <ju.jofrec@duocuc.cl>
2026-03-31 18:40:20 -0300

    Packages creados y metodos controller

---
+++ Ev1/FullStackV1/FullStackV1/src/main/java/com/example/FullStackV1/model/Libro.java
@@ -0,0 +1,11 @@
+package com.example.FullStackV1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class Libro {
+    
+}
